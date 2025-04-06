bfd4b251165e8534d2690249cb0b592c3d84bd2d
Sebastian Araque <cbastian97araque@gmail.com>
2025-04-05 20:55:46 -0500

    ## Componentes 1. Menu lateral (aside)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import HeaderMain from './components/Header/Header'
+import SidebarMenu from './components/SidebarMenu/SidebarMenu'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,6 +9,12 @@ function App() {
   return (
     <>
       <HeaderMain />
+      <main className='main-content'>
+        <SidebarMenu />
+        <section>
+          <h2>Tabla de información</h2>
+        </section>
+      </main>
     </>
   )
 }
