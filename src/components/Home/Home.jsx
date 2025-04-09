31d13cfb036c40d1778467c7cbb6eac7021dfb4d
elbra <elbrac.94@gmail.com>
2025-04-08 19:28:30 -0500

    styles (home): Mejoras en el background y la pantalla de inicio.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>
-      Hola
+    <div className="flex flex-col items-center justify-center gap-y-8 min-h-1/5 p-4">
+      <h1 className="text-6xl font-bold text-gray-800 mt-4">
+        Bienvenido a la Aplicación
+      </h1>
+      <p className="text-gray-600 mb-6">
+        Esta es una aplicación de ejemplo para mostrar la estructura del
+        proyecto.
+      </p>
+      <p className="text-gray-600">
+        Utiliza el menú lateral para navegar por las diferentes secciones.
+      </p>
     </div>
   )
 }
