28ca5b4e606eda59533cd16eb07330e9000a561d
elbra <elbrac.94@gmail.com>
2025-04-08 19:29:14 -0500

    feat (userSettings): Se aagrega page de ajustes de Usuario

---
+++ src/pages/UserSettingsPage.jsx
@@ -0,0 +1,199 @@
+export default function UserSettingsPage() {
+  return (
+    <form>
+      <div className="space-y-12">
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="text-5xl font-semibold text-gray-900">Perfil</h2>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="username"
+                className="block font-medium text-gray-900"
+              >
+                Nombre de usuario
+              </label>
+              <div className="mt-2">
+                <div className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
+                  <input
+                    id="username"
+                    name="username"
+                    type="text"
+                    className="block min-w-0 grow py-1.5 pr-3 pl-1 text-gray-900 placeholder:text-gray-400 focus:outline-none"
+                  />
+                </div>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="about"
+                className="block font-medium text-gray-900"
+              >
+                Acerca de ti
+              </label>
+              <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                  defaultValue={''}
+                />
+              </div>
+              <p className="mt-3 text-gray-600">
+                Escribe algunas frases sobre ti.
+              </p>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="cover-photo"
+                className="block font-medium text-gray-900"
+              >
+                Foto de portada
+              </label>
+              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                <div className="text-center">
+                  <div className="mt-4 flex text-gray-600">
+                    <label
+                      htmlFor="file-upload"
+                      className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:outline-hidden hover:text-indigo-500"
+                    >
+                      <span>Sube un archivo</span>
+                      <input
+                        id="file-upload"
+                        name="file-upload"
+                        type="file"
+                        className="sr-only"
+                      />
+                    </label>
+                    <p className="pl-1">o arrastra y suelta</p>
+                  </div>
+                  <p className="text-gray-600">PNG, JPG, GIF hasta 10MB</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="border-b border-gray-900/10 pb-12">
+          <h2 className="font-semibold text-gray-900">Información personal</h2>
+          <p className="mt-1 text-gray-600">
+            Usa una dirección permanente donde puedas recibir correo.
+          </p>
+
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block font-medium text-gray-900"
+              >
+                Nombre
+              </label>
+              <div className="mt-2">
+                <input
+                  id="first-name"
+                  name="first-name"
+                  type="text"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="last-name"
+                className="block font-medium text-gray-900"
+              >
+                Apellido
+              </label>
+              <div className="mt-2">
+                <input
+                  id="last-name"
+                  name="last-name"
+                  type="text"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-4">
+              <label
+                htmlFor="email"
+                className="block font-medium text-gray-900"
+              >
+                Correo electrónico
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="country"
+                className="block font-medium text-gray-900"
+              >
+                País
+              </label>
+              <div className="mt-2 grid grid-cols-1">
+                <select
+                  id="country"
+                  name="country"
+                  autoComplete="country-name"
+                  className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                >
+                  <option>Colombia</option>
+                  <option>Argentina</option>
+                  <option>Chile</option>
+                  <option>Perú</option>
+                  <option>Venezuela</option>
+                  <option>España</option>
+                  <option>Estados Unidos</option>
+                  <option>Canadá</option>
+                  <option>México</option>
+                </select>
+              </div>
+            </div>
+
+            <div className="col-span-full">
+              <label
+                htmlFor="street-address"
+                className="block font-medium text-gray-900"
+              >
+                Dirección
+              </label>
+              <div className="mt-2">
+                <input
+                  id="street-address"
+                  name="street-address"
+                  type="text"
+                  autoComplete="street-address"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="font-semibold text-gray-900">
+          Cancelar
+        </button>
+        <button className="rounded-md bg-indigo-600 px-3 py-2 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          Guardar
+        </button>
+      </div>
+    </form>
+  )
+}
--- src/theme/MainTemplate.jsx
+++ src/theme/MainTemplate.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import HeaderMain from '../components/Header/Header'
 import SidebarMenu from '../components/SidebarMenu/SidebarMenu'
 
-
-const MainTemplate  = ({ children = null }) => {
+const MainTemplate = ({ children = null }) => {
   const enlaces = [
-    { href: "/usuarios", label: "Usuarios" },
-    { href: "/lenguajes-populares", label: "Lenguajes populares" },
-    { href: "/puntajes", label: "Puntajes" }
-  ];
+    { href: '*', label: 'Inicio' },
+    { href: '/user-settings', label: 'Ajustes de usuario' },
+    { href: '/usuarios', label: 'Usuarios' },
+    { href: '/lenguajes-populares', label: 'Lenguajes populares' },
+    { href: '/puntajes', label: 'Puntajes' },
+  ]
 
   return (
     <>
       <HeaderMain />
-      <main className='main-content'>
+      <main className="main-content">
         <SidebarMenu list={enlaces} />
-        <section>
+        <section className="rounded-2xl bg-white shadow-lg p-4 min-h-screen w-full">
           {children}
         </section>
       </main>
